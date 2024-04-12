d91b64f74db2018b6ff640a2685fd27d75b0fe80
FranciscoCardo09 <pnchocardo@gmail.com>
2024-04-12 10:43:38 -0300

    Ejercicio de division

---
+++ Manejo_de_exceptiones_con_python/Ejercicio4.py
@@ -0,0 +1,21 @@
+def division():
+    while True:
+        try:
+            num1 = float(input("Ingresa el primer número: "))
+            num2 = float(input("Ingresa el segundo número: "))
+
+            resultado = num1 / num2
+
+            print("El resultado de dividir", num1, "y", num2, "es:", resultado)
+
+            continuar = input("¿Quiere seguir sumando valores? (s/n): ")
+
+            if continuar.lower() != 's':
+                break
+        
+        except ValueError:
+            print("Error: Por favor, ingrese solo números.")            
+        except ZeroDivisionError:
+            print("Error: No se puede dividir por cero.")
+
+division()
